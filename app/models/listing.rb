2f52950a8f181d34b86dbec5e8d9a618c57cccc5
Hannah Malm <hannahemalm616@gmail.com>
2022-02-07 17:00:30 -0600

    updated all relationships in all models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,4 @@
 class Listing < ApplicationRecord
+    belongs_to :location
+    has_many :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: true
+    has_many :reservations
+    has_many :reviews
+    has_many :listings, through: :reservations
+    has_many :locations, through: :reservations
 end
