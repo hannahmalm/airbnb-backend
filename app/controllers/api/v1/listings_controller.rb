4efd8d52d24a7d400412f151d7345ffcf64d84d6
Hannah Malm <hannahemalm616@gmail.com>
2022-02-07 17:49:19 -0600

    added index and show in listings

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -1,3 +1,14 @@
 class Api::V1::ListingsController < ApplicationController
 
+    def index
+        @listings = Listings.all
+    
+        render json: @listings
+      end
+    
+      # GET /reviews/1
+      def show
+        render json: @listings, status: 200
+      end
+
 end 
