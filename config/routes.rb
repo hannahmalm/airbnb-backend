aebe842d98b8b4d364881aca716bfe291f2404f3
Hannah Malm <hannahemalm616@gmail.com>
2022-02-07 15:07:37 -0600

    updated routes for all resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
+
+  #UPDATE THESE TO ONLY ALLOW SPECIFIC ROUTES
   namespace :api do 
     namespace :v1 do 
       resources :users
+      resources :listings
+      resources :locations
+      resources :reservations
+      resources :reviews
     end 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
