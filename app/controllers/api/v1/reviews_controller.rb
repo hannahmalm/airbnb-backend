f6a61be9b677841207c7aed8e141ae51ec95da2a
Hannah Malm <hannahemalm616@gmail.com>
2022-02-07 17:47:45 -0600

    added index and show in reviews

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,3 +1,14 @@
 class Api::V1::ReviewsController < ApplicationController
 
+    def index
+        @reviews = Reviews.all
+    
+        render json: @reviews
+      end
+    
+      # GET /users/1
+      def show
+        render json: @reviews, status: 200
+      end
+
 end 
