dcb4f3f4f458fd3366da57bf601b443b939bc809
Hannah Malm <hannahemalm616@gmail.com>
2022-02-07 15:03:32 -0600

    migrated all tables

--- db/migrate/20220207205020_create_locations.rb
+++ db/migrate/20220207205020_create_locations.rb
@@ -3,7 +3,8 @@ class CreateLocations < ActiveRecord::Migration[6.1]
     create_table :locations do |t|
       t.string :city
       t.string :state
-
+      t.string :city_image
+  
       t.timestamps
     end
   end
---
+++ db/schema.rb
@@ -0,0 +1,61 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_02_07_205130) do
+
+  create_table "listings", force: :cascade do |t|
+    t.string "title"
+    t.string "host_name"
+    t.integer "beds"
+    t.integer "baths"
+    t.string "image_url"
+    t.decimal "base_price"
+    t.string "amenity_description"
+    t.integer "location_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "city"
+    t.string "state"
+    t.string "city_image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "listing_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "description"
+    t.integer "user_id"
+    t.integer "listing_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
